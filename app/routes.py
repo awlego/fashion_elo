8ce92008a47441e459bc6e02fe5cb694f508bf62
Alex Wilson <awlego@gmail.com>
2023-08-21 02:48:09 -0700

    now the data for the unselected uid is there too

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # app/routes.py
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from app.models.models import Image
 import random
 from app.views import main  # import the blueprint
@@ -15,12 +15,16 @@ def get_two_random_images():
     random_images = random.sample(all_images, 2) if len(all_images) >= 2 else all_images
     return jsonify([{'uid': img.uid, 'path': img.filepath} for img in random_images])
 
-@main.route('/select_image/<string:image_id>', methods=['GET'])
-def select_image(image_id):
-    print("new image selected")
+@main.route('/select_image', methods=['POST'])
+def select_image():
+    data = request.json
+    print(data)
+    selected_image_id = data['selected']
+    unselected_image_id = data['unselected']
     # TODO: Handle the image selection logic if needed.
     # e.g. increment a counter for the selected image or log the choice
-    print(image_id, " selected")
+    print(selected_image_id, " selected")
+    print(unselected_image_id, " unselected")
 
     # For simplicity, after selecting, we're just getting two new images.
     # You can modify this part based on what action you want to take after a selection.
